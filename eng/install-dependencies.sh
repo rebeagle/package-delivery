50b2f0765595f3b6d3b3912ea1757e52fb511065
Will Brown <will@rebeagle.com>
2020-07-10 18:03:22 -0400

    fix gpg setup

--- eng/install-dependencies.sh
+++ eng/install-dependencies.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-sudo apt install rclone
-
 APTLY_FILE="aptly_${APTLY_VERSION}_amd64.deb"
 curl -LO "https://github.com/aptly-dev/aptly/releases/download/v${APTLY_VERSION}/${APTLY_FILE}"
 sha256sum -c <<< "${APTLY_SHA256}  ${APTLY_FILE}"
-sudo apt install "./${APTLY_FILE}"
+sudo apt-get install "./${APTLY_FILE}" rclone
